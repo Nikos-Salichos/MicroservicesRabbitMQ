a8fe8fcba99f4e2d94a65f0a679d3c380122a5d1
Nikos S <niksal1991@gmail.com>
2022-10-11 08:39:04 +0300

    Implemented RabbitMQBus

---
+++ Infrastructure.Bus/RabbitMQBus.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Core.Bus;
+using Domain.Core.Commands;
+using Domain.Core.Events;
+using MediatR;
+
+namespace Infrastructure.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, THandler>()
+            where T : Event
+            where THandler : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
