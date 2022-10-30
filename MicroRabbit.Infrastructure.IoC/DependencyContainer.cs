b0cf6aa532f4e2cf5103afcc6e1296a210af598a
Nikos S <niksal1991@gmail.com>
2022-10-30 08:11:10 +0200

    Added BankingDbContext in DependencyContainer

--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Bus;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
 using MicroRabbit.Banking.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +21,7 @@ namespace MicroRabbit.Infrastructure.IoC
 
             //Data
             services.AddTransient<IAccountRepository, AccountRepository>();
-
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
