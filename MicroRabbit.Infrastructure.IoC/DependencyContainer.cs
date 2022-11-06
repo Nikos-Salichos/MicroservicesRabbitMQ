ae18877e88e0f304e4094234cb88bb2f3b181b76
Nikos S <niksal1991@gmail.com>
2022-11-06 10:12:12 +0200

    Added TransferCommand to IoC

--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,9 +1,12 @@
 ﻿using Domain.Core.Bus;
 using Infrastructure.Bus;
+using MediatR;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,12 +19,17 @@ namespace MicroRabbit.Infrastructure.IoC
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
 
+            //Domain Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
 
             //Data
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<BankingDbContext>();
+
+            //Events
         }
     }
 }
