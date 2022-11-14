60477078cff713910d235d6486b0e267d871a83e
Nikos S <niksal1991@gmail.com>
2022-11-14 08:13:16 +0200

    Added TransferLog and initial migration to Database

--- MicroRabbit.Transfer.Data/Migrations/20221113180128_Initial Migration.cs
+++ MicroRabbit.Transfer.Data/Migrations/20221113180128_Initial Migration.cs
@@ -2,31 +2,35 @@
 
 #nullable disable
 
-namespace MicroRabbit.Banking.Data.Migrations
+namespace MicroRabbit.Transfer.Data.Migrations
 {
+    /// <inheritdoc />
     public partial class InitialMigration : Migration
     {
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Accounts",
+                name: "TransferLogs",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    AccountType = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    AccountBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                    FromAccount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToAccount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.PrimaryKey("PK_TransferLogs", x => x.Id);
                 });
         }
 
+        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Accounts");
+                name: "TransferLogs");
         }
     }
 }
--- MicroRabbit.Transfer.Domain/Models/TransferLog.cs
+++ MicroRabbit.Transfer.Domain/Models/TransferLog.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string FromAccount { get; set; }
-        public decimal ToAccount { get; set; }
+        public string ToAccount { get; set; }
         public decimal Amount { get; set; }
     }
 }
