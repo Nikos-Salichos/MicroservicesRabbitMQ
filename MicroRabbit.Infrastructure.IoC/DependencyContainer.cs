591fca7657af6f8a62bd6b35340effc2babd404f
Nikos S <niksal1991@gmail.com>
2022-11-16 18:32:06 +0200

    Registered ITransferRepository, TransferRepository

--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -10,7 +10,6 @@ using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Transfer.Application.Interfaces;
 using MicroRabbit.Transfer.Application.Services;
-using MicroRabbit.Transfer.Data.Context;
 using MicroRabbit.Transfer.Data.Repository;
 using MicroRabbit.Transfer.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,7 +34,7 @@ namespace MicroRabbit.Infrastructure.IoC
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<ITransferRepository, TransferRepository>();
             services.AddTransient<BankingDbContext>();
-            services.AddTransient<TransferDbContext>();
+
 
             //Events
         }
