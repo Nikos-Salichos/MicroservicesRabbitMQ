1613520340a5feaa68651afbcba92913c4c4e93f
Nikos S <niksal1991@gmail.com>
2022-11-16 08:18:14 +0200

    Added ITransferService

--- MicroRabbit.Transfer.Application/Services/TransferService.cs
+++ MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Core.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
 
 namespace MicroRabbit.Transfer.Application.Services
 {
-    internal class TransferService
+    public class TransferService : ITransferService
     {
+        private readonly ITransferRepository _transferRepository;
+        private readonly IEventBus _bus;
+
+        public TransferService(ITransferRepository transferRepository, IEventBus bus)
+        {
+            _transferRepository = transferRepository;
+            _bus = bus;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _transferRepository.GetTransferLogs();
+        }
+
+
     }
 }
---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private TransferDbContext _tranferDbContext;
+
+        public TransferRepository(TransferDbContext transferDbContext)
+        {
+            _tranferDbContext = transferDbContext;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _tranferDbContext.TransferLogs;
+        }
+
+    }
+}
+
