05de4348d8d26f8171d6f6dc0cb5c1840f64da8d
Nikos S <niksal1991@gmail.com>
2022-11-18 08:43:30 +0200

    Added TransferEventHandler

--- MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,17 +1,17 @@
-﻿using Domain.Core.Bus;
+﻿
+
+using Domain.Core.Bus;
 using Infrastructure.Bus;
 using MediatR;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
-using MicroRabbit.Banking.Domain.CommandHandlers;
 using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Transfer.Application.Interfaces;
 using MicroRabbit.Transfer.Application.Services;
-using MicroRabbit.Transfer.Data.Context;
-using MicroRabbit.Transfer.Data.Repository;
 using MicroRabbit.Transfer.Domain.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,6 +23,9 @@ namespace MicroRabbit.Infrastructure.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Domain Events
+            services.AddTransient<IEventHandler<TransferCreatedEvent>, Transfer.Domain.EventHandlers.TransferEventHandler>();
 
             //Domain Commands
             services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
---
+++ MicroRabbit.Transfer.Domain/EventHandlers/TransferEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Core.Bus;
+using MicroRabbit.Banking.Domain.Events;
+
+namespace MicroRabbit.Transfer.Domain.EventHandlers
+{
+    public class TransferEventHandler : IEventHandler<TransferCreatedEvent>
+    {
+        public Task Handle(TransferCreatedEvent @event)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
