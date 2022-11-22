9628c77111c3de2566a635f3748dfd62f285a706
Nikos S <niksal1991@gmail.com>
2022-11-22 08:02:31 +0200

    DI ITransferRepository

--- MicroRabbit.Transfer.Domain/EventHandlers/TransferEventHandler.cs
+++ MicroRabbit.Transfer.Domain/EventHandlers/TransferEventHandler.cs
@@ -1,13 +1,15 @@
 ﻿using Domain.Core.Bus;
 using MicroRabbit.Transfer.Domain.Events;
+using MicroRabbit.Transfer.Domain.Interfaces;
 
 namespace MicroRabbit.Transfer.Domain.EventHandlers
 {
     public class TransferEventHandler : IEventHandler<TransferCreatedEvent>
     {
-        public TransferEventHandler()
+        private readonly ITransferRepository _transferRepository;
+        public TransferEventHandler(ITransferRepository transferRepository)
         {
-
+            _transferRepository = transferRepository;
         }
 
         public Task Handle(TransferCreatedEvent @event)
